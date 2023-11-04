0beee564c3589b08e708c3669f2128b6dc0e477f
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2023-11-03 21:14:21 -0300

    Create Exercicio 01.pl

---
+++ PROLOG/Lista 4/Exercicio 01.pl
@@ -0,0 +1,21 @@
+% estrada(número, origem, destino).
+estrada(1, a, b).
+estrada(2, a, d).
+estrada(3, b, c).
+estrada(4, c, d).
+estrada(5, b, e).
+estrada(6, c, f).
+estrada(7, d, f).
+estrada(8, e, f).
+
+% rota(A, B, R).
+rota(A, B, estrada(R, A, B)) :-
+  estrada(R, A, B).
+
+% rotac(R, A).
+rotac(estrada(R, X, A), A) :-
+  estrada(R, X, A).
+
+% rotas(R, A).
+rotas(estrada(R, A, X), A) :-
+  estrada(R, A, X).
