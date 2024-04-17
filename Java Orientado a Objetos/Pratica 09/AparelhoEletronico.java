63d7bd3a9defbc13c126fd48c2ae47d4b823aa8d
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-04-17 16:25:33 -0300

    Create AparelhoEletronico.java

---
+++ Java Orientado a Objetos/Pratica 09/AparelhoEletronico.java
@@ -0,0 +1,50 @@
+package estudosp3;
+
+public class AparelhoEletronico {
+	private String fabricante;
+	private String sistemaCor;
+	private Double preco;
+	
+	public AparelhoEletronico(String fabricante, String sistemaCor, Double preco) throws ExcecaoDadoInvalido {
+		setFabricante(fabricante);
+		setSistemaCor(sistemaCor);
+		setPreco(preco);
+	}
+	
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public String getSistemaCor() {
+		return sistemaCor;
+	}
+	
+	
+	public void setSistemaCor(String sistemaCor) throws ExcecaoDadoInvalido {
+	    if (sistemaCor == null || sistemaCor.isEmpty()) {
+	        throw new ExcecaoDadoInvalido("O sistema de cor não pode ser nulo ou vazio.");
+	    }
+	    if (!sistemaCor.equals("NTSC") && !sistemaCor.equals("PALM")) {
+	        throw new ExcecaoDadoInvalido("O sistema de cor deve ser NTSC ou PALM.");
+	    }
+	    this.sistemaCor = sistemaCor;
+	}
+	
+	
+	public Double getPreco() {
+		return preco;
+	}
+	
+	
+	public void setPreco(Double preco) throws ExcecaoDadoInvalido {
+	    if (preco < 0) {
+	        throw new ExcecaoDadoInvalido("O preço não pode ser negativo.");
+	    }
+	    this.preco = preco;
+	}
+	
+	
+	
+}
