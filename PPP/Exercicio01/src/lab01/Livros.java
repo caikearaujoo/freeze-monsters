4cff252c035cb91790df06a5b5309a2a9f6f158d
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-12-19 11:02:12 -0300

    Add files via upload

---
+++ PPP/Exercicio01/src/lab01/Livros.java
@@ -0,0 +1,7 @@
+package lab01;
+
+class Livros extends Itens{
+	public Livros(String titulo, String autor, int anoLancamento, int numPaginas, String tipo) {
+		super(titulo, autor, anoLancamento, numPaginas, tipo);
+	}
+}
