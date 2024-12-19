6ff47ef6cac5324cdbb06c342e2d3eef5206fd24
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-12-19 18:45:39 -0300

    Delete PPP/Exercicio01 directory

--- PPP/Exercicio01/src/lab01/Revistas.java
+++
@@ -1,8 +0,0 @@
-package lab01;
-
-class Revistas extends Itens{
-		public Revistas(String titulo, String autor, int anoLancamento, int numPaginas, String tipo) {
-			super(titulo, autor, anoLancamento, numPaginas, tipo);
-		}
-	}
-
